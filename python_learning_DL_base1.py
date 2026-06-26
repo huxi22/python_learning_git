b1f3a6f02a5a0934cd245eba60f7f03402867d91
Hu <734762852@qq.com>
2026-06-26 21:04:26 +0800

    modify DL_based1 for learning model-train and test (finished)

--- python_learning_DL_base1.py
+++ python_learning_DL_base1.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader, random_split
 
 class UWB_CRNN(nn.Module):
     def __init__(self, cir_len = 640, num_bins = 200):
@@ -39,14 +40,118 @@ class UWB_CRNN(nn.Module):
         return out
 
 
+def main_pipeline(fullDataSet):
+    # ---------- 模型配置 ---------- #
+    # 硬件配置
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = UWB_CRNN()
+    # 划分数据集
+    total_size = len(fullDataSet)
+    train_size = int(total_size * 0.7)
+    val_size   = int(total_size * 0.1)
+    test_size  = total_size - train_size - val_size
+    train_set, val_set, test_set = random_split(
+        fullDataSet, 
+        [train_size, val_size, test_size]
+    )
+    train_loader = DataLoader(train_set, batch_size=8, shuffle=True)
+    val_loader   = DataLoader(val_set, batch_size=2, shuffle=False)
+    test_loader  = DataLoader(test_set, batch_size=2, shuffle=False)
 
-x = torch.rand(20, 30, 640)
-y = model(x)
+    # 模型和优化器初始化
+    model = UWB_CRNN().to(device)
+    criterion = nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-print(x.shape)
-print(y.shape)
+
+    # ---------- 训练流程 ---------- #
+    print("\n=== 开始训练/验证 ===")
+
+    epochs = 100
+    best_val_loss = float("inf")
+
+    for epoch in range(epochs):
+
+        # ----- 训练集上训练模型 ----- #
+        model.train()
+        train_loss = 0.0
+
+        for inputs, targets in train_loader:
+            # 把输入数据放到device上
+            inputs, targets = inputs.to(device), targets.to(device).float()
+
+            # 模型梯度置为0
+            optimizer.zero_grad()
+
+            # 计算loss
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            # 梯度反向传播
+            loss.backward()
+            optimizer.step()
+        
+            train_loss += loss.item()
+
+        ave_train_loss = train_loss/len(train_loader)
+        
+
+        # ----- 验证集上验证模型训练结果 ----- #
+        model.eval()
+        val_loss = 0.0
+
+        with torch.no_grad():
+            for inputs, targets in val_loader:
+                # 把输入数据放到device上
+                inputs, targets = inputs.to(device), targets.to(device).float()
+
+                # 计算loss
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+
+                val_loss += loss.item()
+
+        ave_val_loss = val_loss/len(val_loader)
+        print(f"Epoch [{epoch+1}/{epochs}], Train loss: {ave_train_loss:.4f}, Val loss: {ave_val_loss:.4f}")
+
+
+        # ----- 保存验证集上最好的模型 ----- #
+        if best_val_loss > ave_val_loss:
+            best_val_loss = ave_val_loss
+            torch.save(model.state_dict(), "best_uwb_crnn.pth")
+
+    print("模型训练完成，可以保存用于部署了。")
+
+
+    # ---------- 测试流程 ---------- #
+    print("\n=== 开始测试 ===")
+
+    # 导入最佳模型参数
+    model.load_state_dict(torch.load("best_uwb_crnn"))
+    model.eval()
+
+    test_loss = 0.0
+
+    with torch.no_grad():
+        for inputs, targets in test_loader:
+            # 把输入数据放到device上
+            inputs, targets = inputs.to(device), targets.to(device).float()
+
+            # 计算loss
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+        
+    ave_test_loss = test_loss/len(test_loader)
+    print(f"Test loss: {ave_test_loss:.4f}")
+
+
+if __name__ == '__main__':
+    main_pipeline(fullDataSet)
+
+
+
 
 
 
